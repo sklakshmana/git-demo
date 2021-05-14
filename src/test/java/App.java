04c2191a2f80dfa4a7ac11fa4f5f66c82264fe21
sklakshmana <sklakshmana@gmail.com>
2021-05-14 21:44:01 +0530

    added if confition to check the data

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -4,6 +4,10 @@ public class App {
 	public void addData(String data)
 	{
 		System.out.println("data -" + data);
+		if (data.equals("Selenium Java"))
+		{
+			System.out.println("Welcome");
+		}
 	}
 
 	public static void main(String[] args) 
