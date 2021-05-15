9bc17333f762658a31aac440859a55ea400a3ea4
sklakshmana <sklakshmana@gmail.com>
2021-05-15 13:56:29 +0530

    added constant class and changer in App.java

--- src/test/java/com/gitDemo/App.java
+++ src/test/java/com/gitDemo/App.java
@@ -1,10 +1,10 @@
+package com.gitDemo;
 
 public class App {
 	
 	public void addData(String data)
 	{
-		System.out.println("data -" + data);
-		if (data.equals("Selenium Java"))
+		if (data.equals(Constant.DATA_TYPE))
 		{
 			System.out.println("Welcome");
 		}
---
+++ src/test/java/com/gitDemo/Constant.java
@@ -0,0 +1,6 @@
+package com.gitDemo;
+
+public class Constant 
+{
+	public static String DATA_TYPE="Selenium Java";
+}
